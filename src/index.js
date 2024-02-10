db9b5a025ccd4bdfb9c7372fbde09dfcc955352f
akshi <neni.akki@gmail.com>
2024-02-10 13:26:12 -0500

    Added project img extraction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+import AddOnSdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
-addOnUISdk.ready.then(() => {
-    console.log("addOnUISdk is ready for use.");
+// Wait for the SDK to be ready
+await AddOnSdk.ready;
 
+// Reference to the active document
+const sdkDocument = AddOnSdk.app;
+
+const response = await AddOnSdk.app.document.createRenditions({
+    range: "currentPage",
+    format: "image/jpeg",
 });
+
+const downloadUrl = URL.createObjectURL(response[0].blob);
+console.log(downloadUrl); 
+document.getElementById("anchor").href = downloadUrl; 
+//document.getElementById("download-button").style.display = 'inline-block';
+
+const imageBlob = response[0].blob; 
+const imageUrl = URL.createObjectURL(imageBlob);
+document.getElementById('image1').src = imageUrl;
+document.getElementById('image2').src = imageUrl;
+document.getElementById('image3').src = imageUrl;
+document.getElementById('image-container').style.display = 'flex';
+
+
