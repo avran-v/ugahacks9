d1203b449384f2ffd7b8ab09ba8d446ff40cef31
misry <dmisry.7.21.04@gmail.com>
2024-09-30 19:57:45 -0400

    alternate colors hex codes, outlined colors, copy to clipboard, table2 layout

--- src/contrast.js
+++ src/contrast.js
@@ -25,14 +25,6 @@ function updateButtonColors(buttonId, foregroundId, backgroundId) {
     : "#" + foregroundColor;
 }
 
-//document.getElementById("foregroundColor").addEventListener("input", function () {
- // updateColorSquare("foregroundColor", "foregroundSquare");
-//});
-
-//document.getElementById("backgroundColor").addEventListener("input", function () {
-  //updateColorSquare("backgroundColor", "backgroundSquare");
-//});
-
 document.getElementById("checkContrast").addEventListener("click", function () {
   console.log("button works")
   const foregroundColor = document.getElementById("foregroundColor").value.trim();
@@ -85,14 +77,65 @@ document.getElementById("checkContrast").addEventListener("click", function () {
             const newDiv = document.createElement("div");
             newDiv.innerHTML = `
               <hr style="border: 2px solid #ddd;">
-              <p style="font-size: 16px; margin-left: 2px;">Look at alternative colors below - pick two colors to see their contrast!</p>
-              <div class="color-square" id="newForeSquare1"></div>
-              <div class="color-square" id="newForeSquare2"></div>
-              <div class="color-square" id="newForeSquare3"></div>
-              <div class="color-square" id="newBackSquare1"></div>
-              <div class="color-square" id="newBackSquare2"></div>
-              <div class="color-square" id="newBackSquare3"></div>
-            `;
+              <p style="font-size: 16px; margin-left: 2px;">Look at alternative colors below!</p>
+              <div style="display: flex; justify-content: space-between;">
+                <div style="flex: 1; margin-right: 10px;">
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newForeSquare1" >
+                  </div>
+                  <div class="color-value1" >
+                      <span class="color-value-text" id="newForeSquare1Hex"></span>
+                      <img id="clipboardIcon1" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                    </div>
+                </div>
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newForeSquare2" >
+                  </div>
+                  <div class="color-value2" >
+                      <span class="color-value-text" id="newForeSquare2Hex"></span>
+                      <img id="clipboardIcon2" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                      </div>
+                </div>
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newForeSquare3" >
+                  </div>
+                  <div class="color-value3" >
+                      <span class="color-value-text" id="newForeSquare3Hex"></span>
+                      <img id="clipboardIcon3" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                    </div>
+                </div>
+              </div>
+
+
+              <!-- Background Colors -->
+              <div style="flex: 1; margin-left: 10px;">
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newBackSquare1" style="margin-bottom: 10px;">
+                  </div>
+                  <div class="color-value4" >
+                      <span class="color-value-text" id="newBackSquare1Hex"></span>
+                      <img id="clipboardIcon4" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                    </div>
+                </div>
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newBackSquare2" >
+                  </div>
+                  <div class="color-value5" >
+                      <span class="color-value-text" id="newBackSquare2Hex"></span>
+                      <img id="clipboardIcon5" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                    </div>
+                </div>
+                <div style="width: 100%; display: flex; flex-direction: row; align-items: center;">
+                  <div class="color-square2" id="newBackSquare3" >
+                  </div>
+                  <div class="color-value6" >
+                      <span class="color-value-text" id="newBackSquare3Hex"></span>
+                      <img id="clipboardIcon6" src="https://cdn-icons-png.flaticon.com/512/60/60990.png" style="width: 15px; height: 15px; cursor: pointer;" />
+                    </div>
+                </div>
+              </div>
+            </div>
+           `;
             document.getElementById("tableResult").appendChild(newDiv);
             newForeSquare1 = document.getElementById("newForeSquare1");
             newBackSquare1 = document.getElementById("newBackSquare1");
@@ -101,27 +144,87 @@ document.getElementById("checkContrast").addEventListener("click", function () {
             newForeSquare3 = document.getElementById("newForeSquare3");
             newBackSquare3 = document.getElementById("newBackSquare3");
           }
+        
           addSuggestedColors(foregroundColor, backgroundColor);
+          let selectedForeSquare;
+          let selectedBackSquare;
+
           newForeSquare1.addEventListener("click", function() {
-            dynamicContrastRatio(newForeSquare1.id)
+              handleForeSquareClick(newForeSquare1);
+              dynamicContrastRatio(newForeSquare1.id);
           });
           newForeSquare2.addEventListener("click", function() {
-            dynamicContrastRatio(newForeSquare2.id)
+              handleForeSquareClick(newForeSquare2);
+              dynamicContrastRatio(newForeSquare2.id);
           });
           newForeSquare3.addEventListener("click", function() {
-            dynamicContrastRatio(newForeSquare3.id)
+              handleForeSquareClick(newForeSquare3);
+              dynamicContrastRatio(newForeSquare3.id);
           });
-          newBackSquare1.addEventListener("click", function() {
-            dynamicContrastRatio(newBackSquare1.id)
+
+          newBackSquare1.addEventListener("click", function () {
+              handleBackSquareClick(newBackSquare1);
+              dynamicContrastRatio(newBackSquare1.id);
           });
-          newBackSquare2.addEventListener("click", function() {
-            dynamicContrastRatio(newBackSquare2.id)
+          newBackSquare2.addEventListener("click", function () {
+              handleBackSquareClick(newBackSquare2);
+              dynamicContrastRatio(newBackSquare2.id);
           });
-          newBackSquare3.addEventListener("click", function() {
-            dynamicContrastRatio(newBackSquare3.id)
+          newBackSquare3.addEventListener("click", function () {
+              handleBackSquareClick(newBackSquare3);
+              dynamicContrastRatio(newBackSquare3.id);
           });
+
+          function handleForeSquareClick(square) {
+              if (selectedForeSquare) {
+                  selectedForeSquare.style.border = "";
+              }
+              square.style.border = "2px solid black";
+              selectedForeSquare = square;
+              console.log(`Selected Foreground Square: ${square.id}`);
+          }
+
+          function handleBackSquareClick(square) {
+              if (selectedBackSquare) {
+                  selectedBackSquare.style.border = "";
+              }
+              square.style.border = "2px solid black";
+              selectedBackSquare = square;
+              console.log(`Selected Background Square: ${square.id}`);
+          }
+        
+        
+          function copyToClipboard(inputId) {
+            var copyText = document.getElementById(inputId);
+            navigator.clipboard.writeText(copyText.textContent);
+            console.log("Copied the text: " + copyText.textContent);
+          }
+
+          document.getElementById("clipboardIcon1").addEventListener("click", function() {
+            copyToClipboard("newForeSquare1Hex");
+          });
+          document.getElementById("clipboardIcon2").addEventListener("click", function() {
+            copyToClipboard("newForeSquare2Hex");
+          });
+          document.getElementById("clipboardIcon3").addEventListener("click", function() {
+            copyToClipboard("newForeSquare3Hex");
+          });
+          document.getElementById("clipboardIcon4").addEventListener("click", function() {
+            copyToClipboard("newBackSquare1Hex");
+          });
+          document.getElementById("clipboardIcon5").addEventListener("click", function() {
+            copyToClipboard("newBackSquare2Hex");
+          });
+          document.getElementById("clipboardIcon6").addEventListener("click", function() {
+            copyToClipboard("newBackSquare3Hex");
+          });
+                  
           const resultDiv = document.getElementById("result");
-          //resultDiv.innerHTML = "Contrast results updated.";
+          resultDiv.innerHTML = `
+          <p style="font-size: 16;">Contrast Ratio: </p>
+          <p style="font-weight: bold;">${data.ratio}:1</p>
+        `;
+          console.log("Contrast Ratio: " + data.ratio);
       })
       .catch((error) => {
           console.error("Error fetching data:", error);
@@ -216,28 +319,30 @@ async function addSuggestedColors(foregroundColor, backgroundColor) {
       ];
     }
 
+    const foreHex1 = hslToHex(foreShades[0][0], foreShades[0][1], foreShades[0][2]);
+    const foreHex2 = hslToHex(foreShades[1][0], foreShades[1][1], foreShades[1][2]);
+    const foreHex3 = hslToHex(foreShades[2][0], foreShades[2][1], foreShades[2][2]);
+
     document.getElementById("newForeSquare1").style.backgroundColor = `hsl(${foreShades[0][0]}, ${foreShades[0][1]}%, ${foreShades[0][2]}%)`;
     document.getElementById("newForeSquare2").style.backgroundColor = `hsl(${foreShades[1][0]}, ${foreShades[1][1]}%, ${foreShades[1][2]}%)`;
     document.getElementById("newForeSquare3").style.backgroundColor = `hsl(${foreShades[2][0]}, ${foreShades[2][1]}%, ${foreShades[2][2]}%)`;
+    
+    document.getElementById("newForeSquare1Hex").textContent = foreHex1;
+    document.getElementById("newForeSquare2Hex").textContent = foreHex2;
+    document.getElementById("newForeSquare3Hex").textContent = foreHex3;
+
+    const backHex1 = hslToHex(backShades[0][0], backShades[0][1], backShades[0][2]);
+    const backHex2 = hslToHex(backShades[1][0], backShades[1][1], backShades[1][2]);
+    const backHex3 = hslToHex(backShades[2][0], backShades[2][1], backShades[2][2]);
 
     document.getElementById("newBackSquare1").style.backgroundColor = `hsl(${backShades[0][0]}, ${backShades[0][1]}%, ${backShades[0][2]}%)`;
     document.getElementById("newBackSquare2").style.backgroundColor = `hsl(${backShades[1][0]}, ${backShades[1][1]}%, ${backShades[1][2]}%)`;
     document.getElementById("newBackSquare3").style.backgroundColor = `hsl(${backShades[2][0]}, ${backShades[2][1]}%, ${backShades[2][2]}%)`;
 
-    // document.getElementById("result").innerHTML += `
-    //   <p style="font-size: 14px;">Suggested Foreground Colors:</p>
-    //   <div style="display: flex;">
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${foreShades[0][0]}, ${foreShades[0][1]}%, ${foreShades[0][2]}%);"></div>
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${foreShades[1][0]}, ${foreShades[1][1]}%, ${foreShades[1][2]}%);"></div>
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${foreShades[2][0]}, ${foreShades[2][1]}%, ${foreShades[2][2]}%);"></div>
-    //   </div>
-    //   <p style="font-size: 14px;">Suggested Background Colors:</p>
-    //   <div style="display: flex;">
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${backShades[0][0]}, ${backShades[0][1]}%, ${backShades[0][2]}%);"></div>
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${backShades[1][0]}, ${backShades[1][1]}%, ${backShades[1][2]}%);"></div>
-    //     <div style="width: 20px; height: 20px; background-color: hsl(${backShades[2][0]}, ${backShades[2][1]}%, ${backShades[2][2]}%);"></div>
-    //   </div>
-    // `;
+    document.getElementById("newBackSquare1Hex").textContent = backHex1;
+    document.getElementById("newBackSquare2Hex").textContent = backHex2;
+    document.getElementById("newBackSquare3Hex").textContent = backHex3;
+
   } catch (error) {
     console.error("There was a problem with fetching color data:", error);
   }
@@ -256,22 +361,22 @@ async function getContrastRatio(newForeColor, newBackColor) {
     })
     .then((data) => {
       const resultDiv = document.getElementById("result2");
-      const aaResult =
+     const aaResult =
         data.AA === "pass"
-          ? '<span style="color: green; font-size="30px;">✓</span>'
-          : '<span style="color: red; font-size="30px;">✗</span>';
+          ? '<span style="color: green; font-size: 30px;">✓</span>'
+          : '<span style="color: red; font-size: 30px;">✗</span>';
       const aaaResult =
         data.AAA === "pass"
-          ? '<span style="color: green; font-size="30px;">✓</span>'
-          : '<span style="color: red; font-size="30px;">✗</span>';
+          ? '<span style="color: green; font-size: 30px;">✓</span>'
+          : '<span style="color: red; font-size: 30px;">✗</span>';
       const aaLargeResult =
         data.AALarge === "pass"
-          ? '<span style="color: green; font-size="30px;">✓</span>'
-          : '<span style="color: red; font-size="30px;">✗</span>';
+          ? '<span style="color: green; font-size: 30px;">✓</span>'
+          : '<span style="color: red; font-size: 30px;">✗</span>';
       const aaaLargeResult =
         data.AAALarge === "pass"
-          ? '<span style="color: green; font-size="30px;">✓</span>'
-          : '<span style="color: red; font-size="30px;">✗</span>';
+          ? '<span style="color: green; font-size: 30px;">✓</span>'
+          : '<span style="color: red; font-size: 30px;">✗</span>';
       const leftbutton = document.getElementById("normalAAImage1");
       const rightbutton = document.getElementById("largeAAAImage1");
       leftbutton.style.backgroundColor = newBackColor;
@@ -294,3 +399,19 @@ async function getContrastRatio(newForeColor, newBackColor) {
       console.error("There was a problem with your fetch operation:", error);
     });
 }
+
+
+
+const hslToHex = (h, s, l) => {
+ s /= 100;
+ l /= 100;
+ const a = s * Math.min(l, 1 - l);
+ const f = (n) => {
+   const k = (n + h / 30) % 12;
+   const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+   return Math.round(255 * color)
+     .toString(16)
+     .padStart(2, "0"); // convert to Hex and prefix "0" if needed
+ };
+ return `#${f(0)}${f(8)}${f(4)}`;
+};
